ced320ee6d01d67d6ef655ee0e39b6fc4ccf579c
sgfanega <steve.fanega.ii@gmail.com>
2024-05-10 03:15:33 -0500

    testing fetching

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,12 @@
-export default function Blog() {
+import { GiConsoleController } from "react-icons/gi";
+import { getAboutUsPageContent } from "../api"
+
+export default async function Blog() {
+  
+  const testData = await getAboutUsPageContent();
+
+  console.log(testData);
+  
   return (
     <main className="blog w-100 h-100 d-flex align-items-center justify-content-center" style={{minHeight: "60dvh"}}>
       <h1 className="display-1 fw-medium text-secondary text-center">Blog Coming soon!</h1>
